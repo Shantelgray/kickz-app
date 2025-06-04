035c4dcce3816a01f0725c3e98d5aeeb13c9bdfa
Shantel <Shantelgray24@gmail.com>
2025-06-04 10:37:55 -0400

    updating html and css

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("http://localhost:3000");
+  const shoeSizes = [6, 6.5, 7, 7.5];
+  const selectSize = document.getElementsById("shoe-size");
 });
+fetch("http://localhost:3000")
+  .then((res) => res.json())
+  .then((sneakers) => {
+    sneakers.forEach((sneaker) => {
+      const options = shoeSizes.map((size) => {
+        const option = document.createElement("option");
+        option.value = size;
+        option.textContent = size;
+        return option;
+      });
+    });
+  });
